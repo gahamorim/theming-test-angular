86cea8cb1c20495d7be62d5a7e4ddd7ef03ec172
Gabriel Amorim <gaahamorim95@gmail.com>
2025-01-02 22:45:25 -0300

    fix: removing unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDividerModule,
-  ],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
